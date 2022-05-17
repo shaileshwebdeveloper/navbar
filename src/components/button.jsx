5ca79beadaf59f4db6484dc7c4c721e1a39c3dd8
shaileshwebdeveloper <shailesh.webdevelopment@gmail.com>
2022-05-18 00:00:35 +0530

    assignment done

---
+++ src/components/button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "../test.css"
+
+
+
+
+
+const Contact = () =>{
+
+    return <button className="btn">Contact</button>
+}
+
+export default Contact
---
+++ src/components/links.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../test.css"
+
+const Links = () => {
+    return <div>
+        <a href="https://www.google.com/" className="a">Services</a>
+        <a href="https://www.google.com/" className="a">Projects</a>
+        <a href="https://www.google.com/" className="a">About</a>
+        </div>
+}
+
+export default Links
